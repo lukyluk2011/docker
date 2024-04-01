18f1d098282ff821105ee2a8f3ca1dc90ed88684
lukyluk2011 <141826409+lukyluk2011@users.noreply.github.com>
2024-04-01 19:36:44 +0200

    Update mujprvniprogram.py

--- mujprvniprogram.py
+++ mujprvniprogram.py
@@ -1,12 +1,25 @@
 print ("cau")
 
-imput ("ted ty (musis bez diakritiky a velkych pismen jako ja): ")
+input ("ted ty (musis bez diakritiky a velkych pismen jako ja): ")
 
 print ("co si myslis ze delam?")
 
-odpoved = imput ("ted ty (musis bez diakritiky a velkych pismen jako ja): ")
+odpoved = input ("ted ty (musis bez diakritiky a velkych pismen jako ja): ")
 
 if odpoved == ("pisu ti"):
         print ("uhadl jsi, jsi dobrej, tak zase priste")
 else:
         print ("chyba, ale nevadi, skus to znova")
+        print ("co si myslis ze delam?")
+        odpoved = input ("ted ty (musis bez diakritiky a velkych pismen jako ja): ")
+        if odpoved == ("pisu ti"):
+                print ("uhadl jsi, jsi dobrej, tak zase priste")
+        else:
+                print ("chyba, ale nevadi, skus to znova")
+                print ("co si myslis ze delam?")
+                odpoved = input ("ted ty (musis bez diakritiky a velkych pismen jako ja): ")
+                if odpoved == ("pisu ti"):
+                        print ("uhadl jsi, jsi dobrej, tak zase priste")
+                else:
+                        print ("chyba")
+                        print ("prohrál jsi")
