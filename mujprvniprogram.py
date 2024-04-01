17a068c0fe8cf4f02a8dcac9ba433816da400bb1
lukyluk2011 <141826409+lukyluk2011@users.noreply.github.com>
2024-04-01 18:55:32 +0200

    Create mujprvniprogram.py

---
+++ mujprvniprogram.py
@@ -0,0 +1,12 @@
+print ("cau")
+
+imput ("ted ty (musis bez diakritiky a velkych pismen jako ja): ")
+
+print ("co si myslis ze delam?")
+
+odpoved = imput ("ted ty (musis bez diakritiky a velkych pismen jako ja): ")
+
+if odpoved == ("pisu ti"):
+        print ("uhadl jsi, jsi dobrej, tak zase priste")
+else:
+        print ("chyba, ale nevadi, skus to znova")
